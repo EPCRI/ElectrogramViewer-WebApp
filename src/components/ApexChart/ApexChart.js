b118125c20082bb9089a4293d1ff210ea166051b
Drew Bernard <dbernard@texasheart.org>
2022-04-12 13:53:13 -0500

    scroll buttons working

--- src/components/ApexChart/ApexChart.js
+++ src/components/ApexChart/ApexChart.js
@@ -26,20 +26,12 @@ ChartJS.register(
   annotationPlugin
 );
 
-// const annotation2 = {
-//   type: 'line',
-//   borderColor: 'green',
-//   borderDash: [6, 6],
-//   borderWidth: 1,
-//   xMax: 5,
-//   xMin: 5,
-//   xScaleID: 'x',
-//   yMax: 0,
-//   yMin: 110,
-//   yScaleID: 'y'
-// };
+const myChartRef = React.createRef();
+const labels = [];
+const decimation = 10;
+const numPointsOnChart = 400;
 
-const annotation3 = {
+const annotation = {
   type: 'line',
   borderColor: 'green',
   borderDash: [6, 6],
@@ -52,49 +44,119 @@ const annotation3 = {
   yScaleID: 'y'
 };
 
-const movechart = {
-  id: 'movechart',
-  afterDraw(chart, args, pluginOptions) {
-    console.log("afterDraw()");
-    const { ctx, chartArea: {left, right, top, bottom, width, height} } = chart;
-
-    const angle = Math.PI / 180;
-
-    ctx.beginPath();
-    ctx.lineWidth = 3;
-    ctx.strokeStyle = 'rgba(102, 102, 102, 0.5)';
-    ctx.fillStyle = 'white';
-    ctx.arc(left, height / 2 + top, 15, angle * 0, angle * 360, false);
-    ctx.stroke();
-    ctx.fill();
-    ctx.closePath();
-  }
-}
-
-const chartpluginsset = [
+const movechart = [
   {
-      afterDatasetDraw: (chart) => {
-        const  { ctx } = chart
-        console.log(ctx);
-        ctx.save()
-        ctx.beginPath()
-        ctx.moveTo(200,200)
-        ctx.lineTo(200,100)
-        ctx.lineTo(20,100)
-        ctx.fillStyle='#E24545'
-        ctx.fill()
+    id: 'movechart',
+
+    afterEvent(chart, args) {
+      const { ctx, canvas, chartArea: {left, right, top, bottom, width, height} } = chart;
+
+      canvas.addEventListener('mousemove', (event) => {
+        const x = args.event.x;
+        const y = args.event.y;
+
+        if (x >= left - 15 && x <= left + 15 && y >= height / 2 + top - 15 && y <= height / 2 + top + 15) {
+          canvas.style.cursor = 'pointer';
+        } else if (x >= right - 15 && x <= right + 15 && y >= height / 2 + top - 15 && y <= height / 2 + top + 15) {
+          canvas.style.cursor = 'pointer';
+        } else {
+          canvas.style.cursor = 'default';
+        }
+      });
+    },
+
+    afterDraw(chart, args, pluginOptions) {
+      const { ctx, chartArea: {left, right, top, bottom, width, height} } = chart;
+  
+      const angle = Math.PI / 180;
+  
+      class CircleChevron {
+        // constructor(x1,y1) {
+
+        // }
+        draw(ctx, x1, pixel) {
+          ctx.beginPath();
+          ctx.lineWidth = 3;
+          ctx.strokeStyle = 'rgba(102, 102, 102, 0.5)';
+          ctx.fillStyle = 'white';
+          ctx.arc(x1, height / 2 + top, 15, angle * 0, angle * 360, false);
+          ctx.stroke();
+          ctx.fill();
+          ctx.closePath();
+    
+          // chevron Arrow Left
+          ctx.beginPath();
+          ctx.lineWidth = 3;
+          ctx.strokeStyle = 'rgba(255, 26, 104, 0.5)';
+          ctx.moveTo(x1 + pixel, height / 2 + top - 7.5);
+          ctx.lineTo(x1 - pixel, height / 2 + top);
+          ctx.lineTo(x1 + pixel, height / 2 + top + 7.5);
+          ctx.stroke();
+          ctx.closePath();
+        }
       }
+
+      let drawCircleLeft = new CircleChevron();
+      drawCircleLeft.draw(ctx, left, 5);
+
+      let drawCircleRight = new CircleChevron();
+      drawCircleRight.draw(ctx, right, -5);
+
+    }
   }
 ];
+
+function mouseClickFunction(event) {
+  console.log("mouseClickFunction()");
+  console.log(event);
+  const myChart = myChartRef.current;
+  const { ctx, canvas, chartArea: {left, right, top, bottom, width, height} } = myChart;
+  const rect = canvas.getBoundingClientRect();
+  const x = event.x;
+  const y = event.y;
+  console.log(`x: ${x}  y: ${y}`);
+
+  if(x >= right - 15 && x <= right + 15 && y >= height / 2 + top - 15 && y <= height / 2 + top + 15) {
+    myChart.options.scales.x.min = myChart.options.scales.x.min + numPointsOnChart;
+    myChart.options.scales.x.max = myChart.options.scales.x.max + numPointsOnChart;
+    if (myChart.options.scales.x.max >= myChart.data.datasets[0].data.length) {
+      myChart.options.scales.x.min = myChart.data.datasets[0].data.length - numPointsOnChart;
+      myChart.options.scales.x.max = myChart.data.datasets[0].data.length;
+    }
+    myChart.update();
+  }
+
+  if(x >= left - 15 && x <= left + 15 && y >= height / 2 + top - 15 && y <= height / 2 + top + 15) {
+    myChart.options.scales.x.min = myChart.options.scales.x.min - numPointsOnChart;
+    myChart.options.scales.x.max = myChart.options.scales.x.max - numPointsOnChart;
+    if (myChart.options.scales.x.max <= 0) {
+      myChart.options.scales.x.min = 0;
+      myChart.options.scales.x.max = numPointsOnChart;
+    }
+    myChart.update();
+  }
+  console.log("xmin: " + myChart.options.scales.x.min + ", xmax: " + myChart.options.scales.x.max);
+}
+
+// function moveScroll() {
+//   console.log("moveScroll()");
+//   canvas.addEventListener('click', mouseClickFunction);
+// }
 
 export const options = {
   maintainAspectRatio: false,
   responsive: true,
   spanGaps: true,
+  animation: false,
+  layout: {
+    padding: {
+      right:18,
+    }
+  },
   scales: {
     x: {
       min: 0,
-      max: 4000,
+      max: numPointsOnChart,
     },
     y: {
       beginAtZero: true,
@@ -106,31 +168,15 @@ export const options = {
     }
   },
   plugins: {
-    chartpluginsset,
+    movechart,
     legend: {
       display: false
     },
+  },
+  onClick: function (evt, element) {
+    mouseClickFunction(evt);
   }
-    // zoom: {
-    //   pan: {
-    //     enabled: true,
-    //     mode: 'x',
-    //   },
-    //   zoom: {
-    //     mode: 'x',
-    //     wheel: {
-    //       enabled: true,
-    //     },
-    //     limits: {
-    //       x: {min: 0, max: 100}
-    //     }
-    //   }
-    // },
 };
-
-const labels = [];
-const decimation = 1;
-const myChartRef = React.createRef();
 
 export const data = {
   labels,
@@ -146,12 +192,11 @@ function extractDataToDatasets() {
     const r = Math.floor(Math.random() * 256);
     const g = Math.floor(Math.random() * 256);
     const b = Math.floor(Math.random() * 256);
-    let data_points = json[channel]
+    let data_points = json[channel];
     let firstPoint = data_points[0];
-    let offset = -firstPoint;
-    // data_points = data_points.filter((_,i) => i % decimation === 0);
+    data_points = data_points.filter((_,i) => i % decimation === 0);
     data_points = data_points.map(element=> (3)*element + 132000 - 12000*(i-1));
-    //data_points = data_points.map(element=> (1)*(element + offset));
+    data_points = data_points.slice(0,20000);
     datasets.push({
       label: channel,
       data: data_points,
@@ -193,7 +238,7 @@ class ApexChart extends React.Component {
             height={"100%"} 
             width={"100%"} 
             options={options}
-            plugins={chartpluginsset}
+            plugins={movechart}
             data={data} />
         </div>
         {/* <div className='tool-box'>
